e06ca921d4787e3d013c795dd8261fbe8d9db31c
duboo <69229809+waryong94@users.noreply.github.com>
2025-04-17 18:57:35 +0900

    Update battery.c

--- boards/shields/nice_oled/nice_epaper/widgets/battery.c
+++ boards/shields/nice_oled/nice_epaper/widgets/battery.c
@@ -60,6 +60,7 @@ static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
 
     sprintf(text, "%i%%", state->battery);
     lv_canvas_draw_text(canvas, 26, 19, 42, &label_right_dsc, text);
+    lv_canvas_draw_text(canvas, 26, 0, 42, &label_right_dsc, "87%");
 }
 
 static void draw_charging_level(lv_obj_t *canvas, const struct status_state *state) {
@@ -73,12 +74,16 @@ static void draw_charging_level(lv_obj_t *canvas, const struct status_state *sta
     sprintf(text, "%i%%", state->battery);
     lv_canvas_draw_text(canvas, 26, 19, 35, &label_right_dsc, text);
     lv_canvas_draw_img(canvas, 62, 21, &bolt, &img_dsc);
+    
+    lv_canvas_draw_text(canvas, 26, 0, 35, &label_right_dsc, "94%");
+    lv_canvas_draw_img(canvas, 62, 2, &bolt, &img_dsc);
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_left_dsc;
     init_label_dsc(&label_left_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_LEFT);
-    lv_canvas_draw_text(canvas, 0, 19, 25, &label_left_dsc, "BAT");
+    lv_canvas_draw_text(canvas, 0, 0, 25, &label_left_dsc, "MST");
+    lv_canvas_draw_text(canvas, 0, 19, 25, &label_left_dsc, "SLV");
 
     if (state->charging) {
         // draw_battery_status_charging_level_big(canvas, state);
